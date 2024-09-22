b1bce220c69b8e81a125eadc85d6e9ca0faac8e9
Olena Stadnyk <olena.lightness@gmail.com>
2024-09-22 12:28:18 +0300

    feat: add show hide sidebar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,16 @@
 /*=============== SHOW SIDEBAR ===============*/
+const showSidebar = (toggleId, sidebarId) => {
+  const toggle = document.getElementById(toggleId),
+    sidebar = document.getElementById(sidebarId)
 
+  if (toggle && sidebar) {
+    toggle.addEventListener('click', () => {
+      /* Show sidebar */
+      sidebar.classList.toggle('show-sidebar')
+    })
+  }
+}
+showSidebar('header-toggle', 'sidebar')
 
 /*=============== LINK ACTIVE ===============*/
 
