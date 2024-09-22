752c688889c4e5a31624599fadc643dc76eca8cd
Olena Stadnyk <olena.lightness@gmail.com>
2024-09-22 12:36:29 +0300

    feat: add active link sidebar

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,6 +13,14 @@ const showSidebar = (toggleId, sidebarId) => {
 showSidebar('header-toggle', 'sidebar')
 
 /*=============== LINK ACTIVE ===============*/
+const sidebarLink = document.querySelectorAll('.sidebar__list a')
+
+function linkColor() {
+  sidebarLink.forEach(l => l.classList.remove('active-link'))
+  this.classList.add('active-link')
+}
+
+sidebarLink.forEach(l => l.addEventListener('click', linkColor))
 
 
 /*=============== DARK LIGHT THEME ===============*/
