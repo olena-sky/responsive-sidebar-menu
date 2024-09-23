7ecc14b2c078e0907e9752c10468babe4a282480
Olena Stadnyk <olena.lightness@gmail.com>
2024-09-23 10:33:17 +0300

    feat: add padding left

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,22 @@
 /*=============== SHOW SIDEBAR ===============*/
-const showSidebar = (toggleId, sidebarId) => {
+const showSidebar = (toggleId, sidebarId, headerId, mainId) => {
   const toggle = document.getElementById(toggleId),
-    sidebar = document.getElementById(sidebarId)
+    sidebar = document.getElementById(sidebarId),
+    header = document.getElementById(headerId),
+    main = document.getElementById(mainId)
 
-  if (toggle && sidebar) {
+  if (toggle && sidebar && header && main) {
     toggle.addEventListener('click', () => {
       /* Show sidebar */
       sidebar.classList.toggle('show-sidebar')
+      /* Add padding header */
+      header.classList.toggle('left-pd')
+      /* Add padding main */
+      main.classList.toggle('left-pd')
     })
   }
 }
-showSidebar('header-toggle', 'sidebar')
+showSidebar('header-toggle', 'sidebar', 'header', 'main')
 
 /*=============== LINK ACTIVE ===============*/
 const sidebarLink = document.querySelectorAll('.sidebar__list a')
